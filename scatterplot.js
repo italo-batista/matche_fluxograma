24562736669c863bee108df9872186fcb4efe8c9
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-03-22 10:53:24 -0300

    Refactoring...

--- scatterplot.js
+++ scatterplot.js
@@ -28,10 +28,9 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .ticks(8);
 
-
 // PLOTS ABOVE
 
-var svg1 = d3.select(".above")
+var svg1 = d3.select(".below")
     .append("svg")
     .attr("class", "svg1")
     .attr("width", width)
@@ -42,15 +41,9 @@ svg1.append("g")
     .attr("transform", "translate(" + yAxisPadding + ",0)")
     .call(yAxis);
 
-plot_above(5);
-plot_above(6);
-plot_above(7);
-plot_above(8);
-
-
 // PLOTS UNDER
 
-var svg2 = d3.select(".below")
+var svg2 = d3.select(".above")
     .append("svg")
     .attr("class", "svg2")
     .attr("width", width)
@@ -61,125 +54,62 @@ svg2.append("g")
     .attr("transform", "translate(" + yAxisPadding + ",0)")
     .call(yAxis);
 
-plot_under(1);
-plot_under(2);
-plot_under(3);
-plot_under(4);
+d3.csv("data.csv", function (error, data) {
 
-function plot_above(periodo) {
+    if (error) throw error;
 
-    d3.csv("data.csv", function (error, data) {
+    data.forEach(function (d) {
+        d.mat = +d.mat,
+            d.periodo = +d.periodo,
+            d.freq = +d.freq
+    });
 
-        if (error) throw error;
+    max_mat = d3.max(data, function (d) {
+        return d.mat;
+    });
+    min_mat = d3.min(data, function (d) {
+        return d.mat;
+    });
 
-        data.forEach(function (d) {
-            d.mat = +d.mat,
-                d.periodo = +d.periodo,
-                d.freq = +d.freq
-        });
+    var xScale = d3.scale.linear()
+        .domain([min_mat, max_mat])
+        .range([padding, box_width - box_padding]);
 
-        var mydata = data.filter(function (d) {
-            return d.periodo === periodo;
-        });
+    /*
+    var brushBox;
 
-        max_mat = d3.max(data, function (d) {
-            return d.mat;
-        });
-        min_mat = d3.min(data, function (d) {
-            return d.mat;
-        });
+    var brush = d3.svg.brush()
+        .x(xScale)
+        .y(yScale)
+        .on("brushstart", brushstart)
+        .on("brush", brushmove)
+        .on("brushend", brushend);
+    */
 
+    for (periodo = 1; periodo <= 8; periodo++) {
+
+        var x0;
+        var mysvg;
         var quinto_periodo = 5;
-        var x0 = (box_width * (periodo - quinto_periodo));
+        var primeiro_periodo = 1;
+        if (periodo <= 4) {
+            mysvg = "svg.svg1";
+            x0 = box_width * (periodo - primeiro_periodo);
+        } else {
+            mysvg = "svg.svg2";
+            x0 = (box_width * (periodo - quinto_periodo));
+        }
 
-        var box = d3.select("svg.svg1")
+        var box = d3.select(mysvg)
             .append("g")
             .attr("width", box_width)
             .attr("height", box_height)
             .attr("transform", "translate(" + x0 + "," + 0 + ")");
-
-        var xScale = d3.scale.linear()
-            .domain([min_mat, max_mat])
-            .range([padding, box_width - box_padding]);
-
-        box.selectAll("circle")
-            .data(mydata)
-            .enter()
-            .append("circle")
-            .attr("cx", function (d) {
-                return xScale(d.mat);
-            })
-            .attr("cy", function (d) {
-                return yScale(d.freq);
-            })
-            .attr("r", circle_radius)
-            .style("fill", function (d) {
-                return color(d.periodo);
-            });
-
-        box.append("text")
-            .attr("class", "label")
-            .attr("y", box_height - 6)
-            .attr("x", box_width / 2 - 20)
-            .attr("font-family","Verdana")
-            .attr("font-size","12")
-            .text(periodo + " periodo");
-
-        d3.select("svg.svg1")
-            .append("rect")
-            .attr("class", "frame")
-            .attr("x", x0 + padding_box)
-            .attr("y", 14.3)
-            .attr("width", box_width - padding_box)
-            .attr("height", height - 35);
-
-        d3.select("svg.svg1")
-            .append("line")
-            .attr("id", "top")
-            .attr("x1", x0 + padding_x0)
-            .attr("x2", x0 + box_width)
-            .attr("y1", yScale(max_freq_periodo[periodo] + padding_line))
-            .attr("y2", yScale(max_freq_periodo[periodo] + padding_line));
-
-    }); // close read data
-
-}
-
-function plot_under(periodo) {
-
-    d3.csv("data.csv", function (error, data) {
-
-        if (error) throw error;
-
-        data.forEach(function (d) {
-            d.mat = +d.mat,
-                d.periodo = +d.periodo,
-                d.freq = +d.freq
-        });
 
         var mydata = data.filter(function (d) {
             return d.periodo === periodo;
         });
 
-        max_mat = d3.max(data, function (d) {
-            return d.mat;
-        });
-        min_mat = d3.min(data, function (d) {
-            return d.mat;
-        });
-
-        var x0 = box_width * (periodo - 1);
-
-        var box = d3.select("svg.svg2")
-            .append("g")
-            .attr("width", box_width)
-            .attr("height", box_height)
-            .attr("transform", "translate(" + x0 + "," + 0 + ")");
-
-        var xScale = d3.scale.linear()
-            .domain([min_mat, max_mat])
-            .range([padding, box_width - box_padding]);
-
         box.selectAll("circle")
             .data(mydata)
             .enter()
@@ -199,11 +129,11 @@ function plot_under(periodo) {
             .attr("class", "label")
             .attr("y", box_height - 6)
             .attr("x", box_width / 2 - 20)
-            .attr("font-family","Verdana")
-            .attr("font-size","12")
+            .attr("font-family", "Verdana")
+            .attr("font-size", "12")
             .text(periodo + " periodo");
 
-        d3.select("svg.svg2")
+        d3.select(mysvg)
             .append("rect")
             .attr("class", "frame")
             .attr("x", x0 + padding_box)
@@ -211,7 +141,7 @@ function plot_under(periodo) {
             .attr("width", box_width - padding_box)
             .attr("height", height - 35);
 
-        d3.select("svg.svg2")
+        d3.select(mysvg)
             .append("line")
             .attr("id", "top")
             .attr("x1", x0 + padding_x0)
@@ -219,7 +149,34 @@ function plot_under(periodo) {
             .attr("y1", yScale(max_freq_periodo[periodo] + padding_line))
             .attr("y2", yScale(max_freq_periodo[periodo] + padding_line));
 
+        // box.call(brush);
+    }
 
-    }); // close read data
+    /*
+    function brushstart(p) {
+        if (brushBox !== this) {
+            d3.select(brushBox).call(brush.clear());
+            xScale.domain([p.x]);
+            yScale.domain([p.y]);
+            brushBox = this;
+        }
+    }
 
-}
+    function brushmove(p) {
+        var e = brush.extent();
+        svg1.selectAll("g").selectAll("circle").classed("hidden", function(d) {
+            return e[0][0] > d[p.x] || d[p.x] > e[1][0]
+                || e[0][1] > d[p.y] || d[p.y] > e[1][1];
+        });
+        svg2.selectAll("g").selectAll("circle").classed("hidden", function(d) {
+            return e[0][0] > d[p.x] || d[p.x] > e[1][0]
+                || e[0][1] > d[p.y] || d[p.y] > e[1][1];
+        });
+    }
+
+    function brushend() {
+        if (brush.empty()) svg2.selectAll(".hidden").classed("hidden", false);
+    }
+    */
+}); // close read data
+
